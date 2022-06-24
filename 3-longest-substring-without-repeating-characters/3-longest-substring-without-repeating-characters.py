9431f51db76c4aa0f404eda8ab8194f087d159b5
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-06-23 21:41:02 -0400

    Time: 139 ms (26.97%), Space: 14 MB (49.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,40 +1,37 @@
 # Optimal Solution
 #
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         if len(s) == 1:
-#             return 1
-#         else:
-#             answer = 0
-#             data = set()
-#             a = 0
-#             b = 0
-#             while b < len(s):
-#                 if s[b] in data:
-#                     data.remove(s[a])
-#                     a += 1
-#                     answer = max(answer, len(data))
-#                 else:
-#                     data.add(s[b])
-#                     b += 1
-#                     answer = max(answer, len(data))
-#         return answer
-            
-            
+# Time Complexity   :   O(N)
+# Space Complexity  :   O(N)
 
-# Brute Force Solution
-#
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) <= 1:
             return len(s)
         answer = 0
-        for i in range(0, len(s)):
-            data = set()
-            j = i
-            while j < len(s) and s[j] not in data:
-                data.add(s[j])
-                j += 1
-                answer = max(answer, len(data))
-        return answer
-        
+        data = {}
+        a = 0
+        for b in range(0, len(s)):
+            if s[b] in data and data[s[b]] >= a:
+                a = data[s[b]] + 1
+            data[s[b]] = b
+            answer = max(answer, b - a + 1)
+        return answer           
+
+# Brute Force Solution
+#
+# Time Complexity   :   O(N^2)
+# Space Complexity  :   O(N)
+#
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) <= 1:
+#             return len(s)
+#         answer = 0
+#         for i in range(0, len(s)):
+#             data = set()
+#             j = i
+#             while j < len(s) and s[j] not in data:
+#                 data.add(s[j])
+#                 j += 1
+#                 answer = max(answer, len(data))
+#         return answer
