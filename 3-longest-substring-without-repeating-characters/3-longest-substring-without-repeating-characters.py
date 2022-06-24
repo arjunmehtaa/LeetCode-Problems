b9abcff9c291f943f88003aaf1d4b2dfa4f29f5c
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-06-23 20:14:31 -0400

    Time: 1921 ms (5.00%), Space: 14.1 MB (13.31%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,24 +1,39 @@
-# Optimal Solution
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        else:
-            answer = 0
+        answer = 0
+        for i in range(0, len(s)):
             data = set()
-            a = 0
-            b = 0
-            while b < len(s):
-                if s[b] in data:
-                    data.remove(s[a])
-                    a += 1
-                    answer = max(answer, len(data))
-                else:
-                    data.add(s[b])
-                    b += 1
-                    answer = max(answer, len(data))
+            j = i
+            while j < len(s) and s[j] not in data:
+                data.add(s[j])
+                j += 1
+                answer = max(answer, len(data))
+            print("data is ", data)
         return answer
+            
+                
+
+# Optimal Solution
+#
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) == 1:
+#             return 1
+#         else:
+#             answer = 0
+#             data = set()
+#             a = 0
+#             b = 0
+#             while b < len(s):
+#                 if s[b] in data:
+#                     data.remove(s[a])
+#                     a += 1
+#                     answer = max(answer, len(data))
+#                 else:
+#                     data.add(s[b])
+#                     b += 1
+#                     answer = max(answer, len(data))
+#         return answer
             
             
 
