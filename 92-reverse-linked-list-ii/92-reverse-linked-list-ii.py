8920c587558d7a36c13068f77a6f6afcbb22015d
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-06-27 01:05:53 -0400

    Time: 40 ms (70.57%), Space: 14.1 MB (51.43%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -4,34 +4,34 @@
 #         self.val = val
 #         self.next = next
 
-def reverseLinkedList(head: ListNode, left: int, right: int) -> ListNode:
-    current = head
-    count = left
-    prev = None
-    prev_tail = prev
-    while current and count <= right:
-        next = current.next
-        current.next = prev
-        prev = current
-        current = next
-        count += 1
-    head.next = current
-    return prev
-
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         count = 1
         current = head
         prev = current
+        
         while count < left:
             prev = current
             current = current.next
+            count += 1     
+            
+        reversed_head = None
+        tail = current
+        
+        while count <= right:
+            next = current.next
+            current.next = reversed_head
+            reversed_head = current
+            current = next
             count += 1
+            
+        prev.next = reversed_head
+        tail.next = current
+        
         if left > 1:
-            prev.next = reverseLinkedList(current, left, right)
             return head
         else:
-            return reverseLinkedList(current, left, right)
+            return reversed_head
         
 
         
