43d378e4f59d4991a21dc6119b8bb60e5f272ed7
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-13 01:42:05 -0400

    Time: 58 ms (31.18%), Space: 13.9 MB (69.68%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,21 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Solution using DFS
+
+def traverseTree(node, visited_levels, level, ans):
+    if node == None:
+        return -1
+    level += 1
+    if level not in visited_levels:
+        visited_levels.append(level)
+        ans.append(node.val)
+    traverseTree(node.right, visited_levels, level, ans)
+    traverseTree(node.left, visited_levels, level, ans)
+    
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        res = []
-        queue = [root]
-        while len(queue):
-            size = len(queue)
-            count = 0
-            while count < size:
-                node = queue.pop(0)
-                count += 1
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            res.append(node.val)
-        return res
+        ans = []
+        traverseTree(root, [], 1, ans)
+        return ans
+
+# Solution using BFS
+#
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         if not root:
+#             return []
+#         res = []
+#         queue = [root]
+#         while len(queue):
+#             size = len(queue)
+#             count = 0
+#             while count < size:
+#                 node = queue.pop(0)
+#                 count += 1
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             res.append(node.val)
+#         return res
