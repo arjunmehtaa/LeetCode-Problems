e4b6d0bd6a4bc5b6eda31e600d8867b1a3397eca
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-14 19:02:10 -0400

    Time: 97 ms (78.01%), Space: 21.5 MB (45.83%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
@@ -5,6 +5,28 @@
 #         self.left = left
 #         self.right = right
 
+# Time Complexity   : O(LOG^2(N))
+# Space Complexity  : O(1)
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        upper_count = 0
+        height = getTreeHeight(root)
+        if height == 0:
+            return 1
+        upper_count += pow(2, height) - 1
+        left = 0
+        right = upper_count
+        while left < right:
+            index = math.ceil((left + right) / 2)
+            if nodeExists(index, height, root):
+                left = index
+            else:
+                right = index - 1
+        return upper_count + (left + 1)
+        
 def getTreeHeight(root):
     node = root
     height = 0
@@ -27,27 +49,6 @@ def nodeExists(index, height, node):
             right = mid - 1
         count += 1
     return node != None
-
-class Solution:
-    def countNodes(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        total = 0
-        height = getTreeHeight(root)
-        if height == 0:
-            return 1
-        total += pow(2, height) - 1
-        left = 0
-        right = total
-        while left < right:
-            index = math.ceil((left + right) / 2)
-            if nodeExists(index, height, root):
-                left = index
-            else:
-                right = index - 1
-        return total + (left + 1)
-        
-        
         
         
         
