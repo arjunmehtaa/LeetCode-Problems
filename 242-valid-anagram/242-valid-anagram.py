7c6a541685401b70ec643b3aff4d152a002d7f4e
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-21 00:39:11 -0400

    Time: 6176 ms (5.04%), Space: 14.4 MB (96.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,16 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        set = {}
-        for i in range(0, len(s)):
-            if s[i] not in set:
-                set[s[i]] = 0
-            set[s[i]] += 1
-            if t[i] not in set:
-                set[t[i]] = 0
-            set[t[i]] -= 1
-        for i in range(0, len(s)):
-            if set[s[i]] != 0:
+        for char in s:
+            if s.count(char) != t.count(char):
                 return False
         return True
         
