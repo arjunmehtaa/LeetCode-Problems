38253220e49676cf99b852eadb75d9225289434a
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-21 00:01:57 -0400

    Update BFS in Graph.py

--- BFS in Graph.py
+++ BFS in Graph.py
@@ -1,15 +1,15 @@
-# BFS in Graph (given Adjecency List)
-    
+# BFS in Graph (given Adjecency List)             
+                
 def traverseBFS(graph):
     queue = [0]
-    values = []
-    seen ={}
-    while(len(queue) > 0):
+    seen = {}
+    ans = []
+    while len(queue):
         vertex = queue.pop(0)
-        values.push(vertex)
         seen[vertex] = true
+        ans.append(vertex)
         connections = graph[vertex]
         for i in range(0, len(connections)):
             connection = connections[i]
             if not seen[connection]:
-                queue.append(i)
+                queue.append(connection)
