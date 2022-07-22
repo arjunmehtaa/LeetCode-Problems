3e5350f66c51be3337d9b8084770c48d4076af69
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-22 02:34:58 -0400

    Time: 130 ms (35.09%), Space: 14.1 MB (38.09%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -2,10 +2,12 @@ directions = [[-1,0], [1,0], [0,-1], [0,1]]
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        queue = [[sr, sc]]
         start_color = image[sr][sc]
+        if start_color == color:
+            return image
+        queue = [[sr, sc]]
         image[sr][sc] = color
-        seen = [[None] * len(image[0]) for k in range(len(image))]
+        #seen = [[None] * len(image[0]) for k in range(len(image))]
         while len(queue) > 0:
             pos = queue.pop(0)
             row = pos[0]
@@ -17,7 +19,7 @@ class Solution:
                     continue
                 if image[next_row][next_col] == start_color:
                     image[next_row][next_col] = color
-                    if not seen[next_row][next_col]:
-                        queue.append([next_row, next_col])
-            seen[row][col] = 1
+                    #if not seen[next_row][next_col]:
+                    queue.append([next_row, next_col])
+            #seen[row][col] = 1
         return image
