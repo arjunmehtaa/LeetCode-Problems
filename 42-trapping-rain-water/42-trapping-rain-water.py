d685f2fb68aae6089892609adab74ff4083ba556
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-26 02:40:30 -0400

    Time: 179 ms (59.64%), Space: 16 MB (41.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,13 +1,11 @@
-# Optimal Solution
-
 class Solution:
     def trap(self, height: List[int]) -> int:
         total = 0
-        a = 0
-        b = len(height) - 1
         max_l = 0
         max_r = 0
-        while(a != b):
+        a = 0
+        b = len(height) - 1
+        while a < b:
             if height[a] <= height[b]:
                 if height[a] > max_l:
                     max_l = height[a]
@@ -21,6 +19,50 @@ class Solution:
                     total += max_r - height[b]
                 b -= 1
         return total
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+# Optimal Solution
+
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         total = 0
+#         a = 0
+#         b = len(height) - 1
+#         max_l = 0
+#         max_r = 0
+#         while(a != b):
+#             if height[a] <= height[b]:
+#                 if height[a] > max_l:
+#                     max_l = height[a]
+#                 elif height[a] < max_l:
+#                     total += max_l - height[a]
+#                 a += 1
+#             else:
+#                 if height[b] > max_r:
+#                     max_r = height[b]
+#                 elif height[b] < max_r:
+#                     total += max_r - height[b]
+#                 b -= 1
+#         return total
     
 # Brute Force Solution
 
