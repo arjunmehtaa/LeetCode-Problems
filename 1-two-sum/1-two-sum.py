8e60a4d08cec7015f3e9a886a8fd3479b58f6b91
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-26 01:57:23 -0400

    Time: 121 ms (44.08%), Space: 15.5 MB (9.23%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,56 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for i in range(0, len(nums)): 
-            if nums[i] in dict:
-                return [dict[nums[i]], i]
+        nums_to_find = {}
+        for i in range(0, len(nums)):
+            if nums[i] in nums_to_find:
+                return [i, nums_to_find[nums[i]]]
             else:
-                num_to_find = target - nums[i]
-                dict[num_to_find] = i
-        return None
+                diff = target - nums[i]
+                nums_to_find[diff] = i
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         dict = {}
+#         for i in range(0, len(nums)): 
+#             if nums[i] in dict:
+#                 return [dict[nums[i]], i]
+#             else:
+#                 num_to_find = target - nums[i]
+#                 dict[num_to_find] = i
+#         return None
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
