3343ad2fc23d015d2a106928d70d99fafc6060c0
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-27 00:11:45 -0400

    Time: 90 ms (65.29%), Space: 14 MB (92.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,21 +1,60 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        if len(s) == 1:
+            return 1
+        a = 0
+        b = 0
+        max_len = 1
+        seen = {}
+        while b < len(s):
+            if s[b] in seen and seen[s[b]] >= a:
+                a = seen[s[b]] + 1
+                seen[s[b]] = b
+            else:
+                seen[s[b]] = b
+            max_len = max(max_len, b-a+1)
+            b += 1
+        return max_len
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 # Optimal Solution
 #
 # Time Complexity   :   O(N)
 # Space Complexity  :   O(N)
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-        answer = 0
-        data = {}
-        a = 0
-        for b in range(0, len(s)):
-            if s[b] in data and data[s[b]] >= a:
-                a = data[s[b]] + 1
-            data[s[b]] = b
-            answer = max(answer, b - a + 1)
-        return answer           
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) <= 1:
+#             return len(s)
+#         answer = 0
+#         data = {}
+#         a = 0
+#         for b in range(0, len(s)):
+#             if s[b] in data and data[s[b]] >= a:
+#                 a = data[s[b]] + 1
+#             data[s[b]] = b
+#             answer = max(answer, b - a + 1)
+#         return answer           
 
 # Brute Force Solution
 #
