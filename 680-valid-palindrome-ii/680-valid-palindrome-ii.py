2a5c61a3fcb04937f4fcf5093f3da0fd0f046db4
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-27 00:49:58 -0400

    Time: 154 ms (80.20%), Space: 14.5 MB (88.16%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,20 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if s == s[::-1]:
-            return True
-        flag = False
         a = 0
         b = len(s) - 1
         while a <= b:
             if s[a] != s[b]:
-                a_removed = s[:a] + s[a+1:]
-                b_removed = s[:b] + s[b+1:]
-                if a_removed == a_removed[::-1] or b_removed == b_removed[::-1]:
-                    return True
-                else:
-                    return False
+                x = s[a+1:b+1]
+                y = s[a:b]
+                return x == x[::-1] or y == y[::-1]
             a += 1
             b -= 1
+        return True
         
         
         
