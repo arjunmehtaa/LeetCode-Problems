9f519a4e53a8b3394a7bddf9e48ee461d7ab14ef
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-27 00:50:18 -0400

    Time: 135 ms (86.41%), Space: 14.5 MB (45.53%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,51 +1,17 @@
-class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        a = 0
-        b = len(s) - 1
-        while a <= b:
-            if s[a] != s[b]:
-                x = s[a+1:b+1]
-                y = s[a:b]
-                return x == x[::-1] or y == y[::-1]
-            a += 1
-            b -= 1
-        return True
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 # Optimal Solution
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 
-# class Solution:
-#     def validPalindrome(self, s: str) -> bool:
-#         a = 0
-#         b = len(s) - 1
-#         while a < b:
-#             if s[a] != s[b]:
-#                 x = s[a+1: b+1]
-#                 y = s[a: b]
-#                 return x == x[::-1] or y == y[::-1]
-#             a += 1
-#             b -= 1
-#         return True
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        a = 0
+        b = len(s) - 1
+        while a < b:
+            if s[a] != s[b]:
+                x = s[a+1: b+1]
+                y = s[a: b]
+                return x == x[::-1] or y == y[::-1]
+            a += 1
+            b -= 1
+        return True
