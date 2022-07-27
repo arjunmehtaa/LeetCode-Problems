1c02c4068e00ccc3fd2af20168c90eef0562c8e6
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-26 23:47:15 -0400

    Time: 27 ms (98.28%), Space: 13.9 MB (23.89%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,71 +1,35 @@
-class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        a = getFinalString(s)
-        b = getFinalString(t)
-        return a == b
-        
-def getFinalString(s):
-    a = ""
-    for i in range(0, len(s)):
-        if s[i] == "#":
-            a = a[:-1]
-        else:
-            a += s[i]
-    return a
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 # Optimal Solution
 
-# class Solution:
-#     def backspaceCompare(self, s: str, t: str) -> bool:
-#         a = len(s) - 1
-#         b = len(t) - 1
-#         while a >= 0 or b >= 0:
-#             if a >= 0 and s[a] == "#":
-#                 steps = 2
-#                 while steps > 0:
-#                     a -= 1
-#                     steps -= 1
-#                     if a >= 0 and s[a] == "#":
-#                         steps += 2
-#             if t[b] == "#" and b >= 0:
-#                 steps = 2
-#                 while steps > 0:
-#                     b -= 1
-#                     steps -= 1
-#                     if b >= 0 and t[b] == "#":
-#                         steps += 2
-#             if a >= 0 and b >= 0:
-#                 if s[a] != t[b]:
-#                     return False               
-#                 else:
-#                     a -= 1
-#                     b -= 1
-#             elif a < 0 and b < 0:
-#                 return True
-#             else:
-#                 return False
-#         return True
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        a = len(s) - 1
+        b = len(t) - 1
+        while a >= 0 or b >= 0:
+            if a >= 0 and s[a] == "#":
+                steps = 2
+                while steps > 0:
+                    a -= 1
+                    steps -= 1
+                    if a >= 0 and s[a] == "#":
+                        steps += 2
+            if t[b] == "#" and b >= 0:
+                steps = 2
+                while steps > 0:
+                    b -= 1
+                    steps -= 1
+                    if b >= 0 and t[b] == "#":
+                        steps += 2
+            if a >= 0 and b >= 0:
+                if s[a] != t[b]:
+                    return False               
+                else:
+                    a -= 1
+                    b -= 1
+            elif a < 0 and b < 0:
+                return True
+            else:
+                return False
+        return True
 
 # Brute Force Solution
 #
