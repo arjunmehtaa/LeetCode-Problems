c6fea8918a31228c3dca592f65bc2e5dda5f337a
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-27 23:56:58 -0400

    Time: 35 ms (95.13%), Space: 14.5 MB (92.36%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -7,23 +7,24 @@ class Node:
         self.next = next
         self.child = child
 """
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         current = head
         while current:
             if current.child:
-                next = current.next
+                old_next = current.next
                 current.next = current.child
-                current.next.prev = current
                 current.child = None
-                last = current.next
-                while last.next:
-                    last = last.next
-                last.next = next
-                if next:
-                    next.prev = last
+                current.next.prev = current
+                active = current
+                while active.next:
+                    active = active.next
+                active.next = old_next
+                if old_next:
+                    old_next.prev = active
             current = current.next
         return head
-
         
