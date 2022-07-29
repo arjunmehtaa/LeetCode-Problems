2142a60ec81551148b2a2d9bf413bcda6c390cd5
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-07-29 01:27:00 -0400

    Time: 870 ms (12.79%), Space: 27.1 MB (6.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,45 +1,27 @@
-def quickSelect(nums, start, end, k):
+def quickSelect(nums: List[int], start, end, k):
+    if end <= start:
+        return
+    mid = (end + start) // 2
     pivot = end
+    nums[mid], nums[pivot] = nums[pivot], nums[mid]
     i = start
     j = start
-    for j in range(start, pivot):
-        if nums[j] < nums[pivot]:
+    while(j != pivot):
+        if(nums[j] < nums[pivot]):
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
+            j += 1
+        else:
+            j += 1
     nums[i], nums[pivot] = nums[pivot], nums[i]
-    if i == (len(nums)-k):
+    if i == len(nums) - k:
         return
-    if i < (len(nums)-k):
-        quickSelect(nums, i+1, end, k)
-    else:
+    elif i > len(nums) - k:
         quickSelect(nums, start, i-1, k)
-            
+    else: 
+        quickSelect(nums, i+1, end, k)
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        quickSelect(nums, 0, len(nums)-1, k)
+        quickSelect(nums, 0, len(nums) - 1, k)
         return nums[len(nums) - k]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
