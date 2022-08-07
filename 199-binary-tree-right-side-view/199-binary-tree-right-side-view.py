c480a935b214ef462851b15ad5ff4f06f083d60f
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-06 21:56:44 -0400

    Time: 25 ms (99.55%), Space: 14 MB (23.14%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,44 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Solution using DFS
-#
-# Time Complexity   : O(N)
-# Space Complexity  : O(N)
-
-def traverseTree(node, level, ans):
-    if node == None:
-        return -1
-    level += 1
-    if level > len(ans):
-        ans.append(node.val)
-    traverseTree(node.right, level, ans)
-    traverseTree(node.left, level, ans)
-    
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
-        traverseTree(root, 0, ans)
+        visited = []
+        traverse(root, 0, visited, ans)
         return ans
-
-# Solution using BFS
-#
-# class Solution:
-#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-#         if not root:
-#             return []
-#         res = []
-#         queue = [root]
-#         while len(queue):
-#             size = len(queue)
-#             count = 0
-#             while count < size:
-#                 node = queue.pop(0)
-#                 count += 1
-#                 if node.left:
-#                     queue.append(node.left)
-#                 if node.right:
-#                     queue.append(node.right)
-#             res.append(node.val)
-#         return res
+        
+def traverse(node, level, visited, ans):
+    if not node:
+        return
+    if level not in visited:
+        ans.append(node.val)
+        visited.append(level)
+    level += 1
+    traverse(node.right, level, visited, ans)
+    traverse(node.left, level, visited, ans)
+    
+    
+        
