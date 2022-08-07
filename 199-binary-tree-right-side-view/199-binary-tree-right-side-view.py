dfffb65f91356eefe7b6a785d3d1d08f68a55740
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-06 22:02:08 -0400

    Time: 53 ms (48.28%), Space: 13.8 MB (70.33%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,20 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-        traverse(root, 0, ans)
-        return ans
-        
-def traverse(node, level, ans):
-    if not node:
-        return
+
+# Solution using DFS
+#
+# Time Complexity   : O(N)
+# Space Complexity  : O(N)
+
+def traverseTree(node, level, ans):
+    if node == None:
+        return -1
     level += 1
     if level > len(ans):
         ans.append(node.val)
-    traverse(node.right, level, ans)
-    traverse(node.left, level, ans)
+    traverseTree(node.right, level, ans)
+    traverseTree(node.left, level, ans)
     
-    
-        
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        traverseTree(root, 0, ans)
+        return ans
+
+# Solution using BFS
+#
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         if not root:
+#             return []
+#         res = []
+#         queue = [root]
+#         while len(queue):
+#             size = len(queue)
+#             count = 0
+#             while count < size:
+#                 node = queue.pop(0)
+#                 count += 1
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             res.append(node.val)
+#         return res
