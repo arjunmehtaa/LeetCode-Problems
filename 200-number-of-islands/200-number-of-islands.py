9aabdd0683199ceb3ee0318511befd67602206f7
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-08 00:02:29 -0400

    Time: 439 ms (63.89%), Space: 16.3 MB (80.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,35 +1,40 @@
-# Time Complexity   : O()
-# Space Complexity  : O()
-
-directions = [[-1,0], [0,1], [1,0], [0,-1]]
+directions = [[-1,0], [1,0], [0,-1], [0,1]]
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0:
-            return False
-        islands = 0
+        if not grid:
+            return 0
+        count = 0
         for i in range(0, len(grid)):
             for j in range(0, len(grid[0])):
                 if grid[i][j] == "1":
-                    islands += 1
+                    count += 1
                     grid[i][j] = "0"
-                    queue = [[i,j]]
-                    while len(queue) > 0:
-                        pos = queue.pop(0)
-                        row = pos[0]
-                        col = pos[1]
-                        for k in range(0, len(directions)):
-                            current_dir = directions[k]
-                            next_row = row + current_dir[0]
-                            next_col = col + current_dir[1]
-                            if next_row < 0 or next_col < 0 or next_row >= len(grid) or next_col >= len(grid[0]):
-                                continue
-                            if grid[next_row][next_col] == "1":
-                                queue.append([next_row, next_col])
-                                grid[next_row][next_col] = "0"
-        return islands
-                                
-                                
-                            
-                        
+                    traverseIsland(grid, i , j)
+        return count
+        
+def traverseIsland(grid, i, j):
+    queue = [[i, j]]
+    while len(queue) > 0:
+        element = queue.pop(0)
+        row = element[0]
+        col = element[1]
+        for direction in directions:
+            next_row = row + direction[0]
+            next_col = col + direction[1]
+            if next_row < 0 or next_col < 0 or next_row >= len(grid) or next_col >= len(grid[0]):
+                continue
+            if grid[next_row][next_col] == "1":
+                grid[next_row][next_col] = "0"
+                queue.append([next_row, next_col])
+            
+            
+            
+            
     
+
+        
+# 1 0 0 1
+# 0 0 0 1      
+# 0 0 0 0     expected output = 4
+# 1 0 0 1
