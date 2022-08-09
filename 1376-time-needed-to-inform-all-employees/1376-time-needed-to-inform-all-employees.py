794ce83e03fec674bce7e41f4bee598713cb7666
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-09 01:37:57 -0400

    Time: 2010 ms (60.98%), Space: 37.7 MB (84.91%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,18 +1,21 @@
+# Time Complexity   : O(N)
+# Space Complexity  : O(N)
+
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         adjList = {}
         for i in range(0, len(manager)):
-            if manager[i] not in adjList:
-                adjList[manager[i]] = [i]
-            else:
+            if manager[i] in adjList:
                 adjList[manager[i]].append(i)
-        return dfs(adjList, headID, informTime)
-        
-def dfs(adjList, current, informTime):
+            else:
+                adjList[manager[i]] = [i]
+        return dfs(informTime, adjList, headID)
+
+def dfs(informTime, adjList, current):
     max_time = 0
     if current in adjList:
         subs = adjList[current]
         for i in range(0, len(subs)):
-            branch_time = dfs(adjList, subs[i], informTime)
-            max_time = max(branch_time, max_time)
-    return informTime[current] + max_time
+            branch_time = dfs(informTime, adjList, subs[i])
+            max_time = max(max_time, branch_time)
+    return max_time + informTime[current]
