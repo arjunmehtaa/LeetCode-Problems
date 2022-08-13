af767b2fd7cd36b6c167e33336c884473ef65f8c
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-13 17:51:00 -0400

    Time: 1675 ms (5.01%), Space: 89.4 MB (6.27%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,10 +1,21 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
+        dp = {}
         for i in range(0, len(s)):
             for j in range(i, len(s)):
                 string = s[i:j+1]
-                if string == string[::-1]:
+                if isPalindrome(s, i, j, dp):
                     count += 1
         return count
+    
+def isPalindrome(s, i, j, dp):
+    if (i, j) in dp:
+        return dp[(i, j)]
+    if i >= j:
+        return True
+    if s[i] == s[j]:
+        dp[(i, j)] = isPalindrome(s, i+1, j-1, dp)
+        return dp[(i, j)]
+        
         
