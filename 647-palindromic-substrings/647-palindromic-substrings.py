c350172d649f31766ccfec771826cf649a208c6e
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-13 18:15:23 -0400

    Time: 571 ms (16.95%), Space: 109.3 MB (5.73%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,60 +1,25 @@
 class Solution:
-    def countSubstrings(self, s):
-        if not s:
-            return 0
-
+    def countSubstrings(self, s: str) -> int:
+        dp = {}
         n = len(s)
-        table = [[False for x in range(n)] for y in range(n)]
         count = 0
-
-        # Every isolated char is a palindrome
-        for i in range(n):
-            table[i][i] = True
+        for i in range(0, len(s)):
+            dp[(i,i)] = True
             count += 1
-
-        # Check for a window of size 2
-        for i in range(n-1):
+        for i in range(0, len(s) - 1):
             if s[i] == s[i+1]:
-                table[i][i+1] = True
+                dp[(i, i+1)] = True
                 count += 1
-
-        # Check windows of size 3 and more
-        for k in range(3, n+1):
-            for i in range(n-k+1):
-                j = i+k-1
-                if table[i+1][j-1] and s[i] == s[j]:
-                    table[i][j] = True
+            else:
+                dp[(i, i+1)] = False
+        for k in range(3, n + 1):
+            for i in range(0, n - k + 1):
+                j = i + k - 1
+                if dp[(i+1, j-1)] and s[i] == s[j]:
+                    dp[i, j] = True
                     count += 1
-
+                else:
+                    dp[i, j] = False
         return count
-    
-    
-    
-    
-    
-    
-    
-    # def countSubstrings(self, s: str) -> int:
-    #     dp = {}
-    #     n = len(s)
-    #     count = 0
-    #     for i in range(0, len(s)):
-    #         dp[(i,i)] = True
-    #         count += 1
-    #     for i in range(0, len(s) - 1):
-    #         if s[i] == s[i+1]:
-    #             dp[(i, i+1)] = True
-    #             count += 1
-    #         else:
-    #             dp[(i, i+1)] = False
-    #     for k in range(3, n + 1):
-    #         for i in range(0, n - k + 1):
-    #             j = i + k - 1
-    #             if dp[(i+1, j-1)] and s[i] == s[j]:
-    #                 dp[i, j] = True
-    #                 count += 1
-    #             else:
-    #                 dp[i, j] = False
-    #     return count
         
         
