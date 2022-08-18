af398f61e010524449de8dd491ddbfc2633d089c
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-18 18:52:10 -0400

    Time: 60 ms (89.91%), Space: 17.5 MB (67.21%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -4,20 +4,40 @@
 #         self.val = x
 #         self.next = None
 
+# Optimal Solution
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return False
         a = head
-        b = head.next
-        while b.next:
-            if a == b:
-                return True
+        b = head
+        if head == None:
+            return False
+        while True:
             a = a.next
             b = b.next
-            if b.next:
-                b = b.next
-            else:
+            if not b or not b.next:
                 return False
-        return False
-        
+            else:
+                b = b.next
+            if a == b:
+                break;
+        return True
+
+# Brute Force Solution
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         current = head
+#         data = set()
+#         if current == None:
+#             return False
+#         while current not in data:
+#             if not current.next:
+#                 return False
+#             data.add(current)
+#             current = current.next
+#         return True
