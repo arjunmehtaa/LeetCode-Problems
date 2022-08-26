8713ac44f76b09e807f608164211fac3e657b6a0
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-26 10:43:31 -0400

    Time: 221 ms (48.21%), Space: 14.4 MB (99.32%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,17 +1,17 @@
-# Optimal Solution
-#
-# Time Complexity: O(N)
-# Space Complexity: O(1)
-
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         a = 0
         b = len(s) - 1
+        flag = False
         while a < b:
             if s[a] != s[b]:
                 x = s[a+1: b+1]
-                y = s[a: b]
+                y = s[a:b]
                 return x == x[::-1] or y == y[::-1]
             a += 1
             b -= 1
         return True
+                
+                
+            
+        
