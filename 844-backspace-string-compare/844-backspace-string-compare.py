0a1232e98a6710ccd00684ca82c702e58ac3dbc8
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-26 10:13:31 -0400

    Time: 39 ms (77.92%), Space: 13.9 MB (74.96%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,16 +1,33 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        x = generateString(s)
-        y = generateString(t)
-        if x == y:
-            return True
-        return False
+        a = len(s) - 1
+        b = len(t) - 1
+        while a >= 0 or b >= 0:
+            if a >= 0 and s[a] == "#":
+                steps = 2
+                while steps > 0:
+                    a -= 1
+                    steps -= 1
+                    if a >= 0 and s[a] == "#":
+                        steps += 2
+            if b >= 0 and t[b] == "#":
+                steps = 2
+                while steps > 0:
+                    b -= 1
+                    steps -=  1
+                    if b >= 0 and t[b] == "#":
+                        steps += 2
+            if a >= 0 and b >= 0:
+                if s[a] != t[b]:
+                    return False
+                else:
+                    a -= 1
+                    b -= 1
+            elif a < 0 and b < 0:
+                return True
+            else:
+                return False
+        return True
+                    
+                
         
-def generateString(s):
-    a = ""
-    for i in range(0, len(s)):
-        if s[i] == "#":
-            a = a[:-1]
-        else:
-            a += s[i]
-    return a
