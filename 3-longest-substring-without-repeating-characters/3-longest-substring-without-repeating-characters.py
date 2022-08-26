7e80910c5f78a3774793e210f783d6538117dd3f
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-26 10:31:06 -0400

    Time: 76 ms (82.19%), Space: 14 MB (50.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,17 +1,37 @@
+# Optimal Solution
+#
+# Time Complexity   :   O(N)
+# Space Complexity  :   O(N)
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) <= 1:
             return len(s)
-        dict = {}
-        a = 0
-        b = 0
         answer = 0
-        while b < len(s):
-            if s[b] in dict and dict[s[b]] >= a:
-                a = dict[s[b]] + 1
-            dict[s[b]] = b
+        data = {}
+        a = 0
+        for b in range(0, len(s)):
+            if s[b] in data and data[s[b]] >= a:
+                a = data[s[b]] + 1
+            data[s[b]] = b
             answer = max(answer, b - a + 1)
-            b += 1
-        return answer
-        
-        
+        return answer           
+
+# Brute Force Solution
+#
+# Time Complexity   :   O(N^2)
+# Space Complexity  :   O(N)
+#
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) <= 1:
+#             return len(s)
+#         answer = 0
+#         for i in range(0, len(s)):
+#             data = set()
+#             j = i
+#             while j < len(s) and s[j] not in data:
+#                 data.add(s[j])
+#                 j += 1
+#                 answer = max(answer, len(data))
+#         return answer
