6f64dd5340f4405878d789bea0e78249921a7149
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-08-29 01:25:31 -0400

    Time: 128 ms (62.48%), Space: 24.2 MB (28.37%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return None
+        
+        # Find the middle
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        current = slow.next
+        
+        # Reverse second half
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        slow.next = None
+        
+        # Merge the two lists
+        head1 = head
+        head2 = prev
+        while head2:
+            next = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = next
+        return head
+    
+    
+    
+    
+    
+    
+    
+    
+    
