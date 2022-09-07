52fefed3ca59d9c09a982c25d0a1915948df704e
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-07 19:34:54 -0400

    Time: 75 ms (10.78%), Space: 14 MB (29.35%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        stack = []
+        for j in range(0, len(matrix[0])):
+            for i in range(len(matrix) - 1, -1, -1):
+                stack.append(matrix[i][j])
+        print(stack)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                matrix[i][j] = stack.pop(0)
+            
