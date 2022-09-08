bcf72510bc24cc55cfa28aed41776dbda5a66857
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-08 01:27:01 -0400

    Time: 965 ms (64.90%), Space: 13.9 MB (90.20%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,3 +1,6 @@
+# Time Complexity	: O(N^2)
+# Space Complexity	: O(1)
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
@@ -5,13 +8,14 @@ class Solution:
         for i in range(len(s)):
             l = i
             r = i
+            # odd length
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
                     res = s[l:r+1]
                     resLen = r - l + 1
                 l -= 1
                 r += 1
-            
+            # even length
             l = i
             r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
