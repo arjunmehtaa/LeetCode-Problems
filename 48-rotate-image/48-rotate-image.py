5e550ff23c220668aba721189cfdf5752cd2497a
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-07 20:06:39 -0400

    Time: 74 ms (12.07%), Space: 13.8 MB (73.90%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,11 +3,21 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        stack = []
-        for j in range(0, len(matrix[0])):
-            for i in range(len(matrix) - 1, -1, -1):
-                stack.append(matrix[i][j])
+        
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrix[i][j] = stack.pop(0)
+            for j in range(i, len(matrix[0])):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+                
+        for row in matrix:
+            row.reverse()
+        
+        # stack = []
+        # for j in range(0, len(matrix[0])):
+        #     for i in range(len(matrix) - 1, -1, -1):
+        #         stack.append(matrix[i][j])
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         matrix[i][j] = stack.pop(0)
             
