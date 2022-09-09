e7d8e02e7da4fa696e34cbca38ba75aa348ba30d
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 14:56:38 -0400

    Time: 77 ms (5.18%), Space: 13.8 MB (83.20%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -2,58 +2,26 @@ directions = [[0,1], [1,0], [0,-1], [-1,0]]
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        seen = [[False] * len(matrix[0]) for _ in range(len(matrix))]
-        size = len(matrix)*len(matrix[0])
-        ans = []
-        i = 0
-        j = 0
-        queue = []
-        counter = 0
-        finished = False
-        while not finished:
+        total = len(matrix)*len(matrix[0]) - 1
+        ans = [matrix[0][0]]
+        row, col = 0, 0
+        seen = set()
+        seen.add((0, 0))
+        while total > 0:
             for direction in directions:
-                counter += 1
-                queue = [[i, j]]
-                while len(queue) > 0:
-                    element = queue.pop(0)
-                    row = element[0]
-                    col = element[1]
-                    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
-                        continue
-                    if not seen[row][col]:
-                        ans.append(matrix[row][col])
-                        if len(ans) == size:
-                            finished = True
-                        seen[row][col] = True
-                        i = row
-                        j = col
-                    queue.append([row + direction[0], col + direction[1]])
+                row += direction[0]
+                col += direction[1]
+                while (row >= 0 
+                       and col >= 0 
+                       and row < len(matrix) 
+                       and col < len(matrix[0]) 
+                       and (row, col) not in seen):
+                    ans.append(matrix[row][col])
+                    total -= 1
+                    seen.add((row, col))
+                    row += direction[0]
+                    col += direction[1]
+                row -= direction[0]
+                col -= direction[1]
         return ans
-    
-    
-# # BFS in 2D Array
-
-# # Time Complexity   : O(N)
-# # Space Complexity  : O(N)
-
-# # Note: If values are allowed to be duplicate, seen functionality would have to change
-# # to a an array of boolean values, where we can check if seen[i][j] is true
-
-# class Solution:
-#     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-#         seen = set()
-#         ans = []
-#         queue = [[0,0]]
-#         while len(queue) > 0:
-#             position = queue.pop(0)
-#             i = position[0]
-#             j = position[1]
-#             if i < 0 or j < 0 or i >= len(matrix) or j >= len(matrix[0]) or matrix[i][j] in seen:
-#                 continue
-#             ans.append(matrix[i][j])
-#             seen.add(matrix[i][j])
-#             queue.append([i-1, j])
-#             queue.append([i, j+1])
-#             queue.append([i+1, j])
-#             queue.append([i, j-1])
-#         print(ans)
+                    
