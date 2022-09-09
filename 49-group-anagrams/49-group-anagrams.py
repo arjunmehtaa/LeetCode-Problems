b20f52fa6e8541090b3b234cbb7f910d91a1ca41
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 18:05:25 -0400

    Time: 121 ms (82.45%), Space: 20 MB (9.44%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,36 @@
+# Let N be average length of word and M words
+
+# Optimal Solution
+# Time Complexity	: O(M * N LOG(N))
+# Space Complexity	: O(M * N LOG(N))
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagramMap = {}
+        answer = defaultdict(list)
         for word in strs:
-            sortedWord = "".join(sorted(word))
-            if sortedWord in anagramMap:
-                anagramMap[sortedWord].append(word)
-            else:
-                anagramMap[sortedWord] = [word]
-        return anagramMap.values()
-            
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord("a")] += 1
+            answer[tuple(count)].append(word)
+        return answer.values()
+        
+        
+        
+        
+        
+        
+# Brute Force Solution
+# Time Complexity	: O(M * N LOG(N))
+# Space Complexity	: O(M * N LOG(N))
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagramMap = {}
+#         for word in strs:
+#             sortedWord = "".join(sorted(word))
+#             if sortedWord in anagramMap:
+#                 anagramMap[sortedWord].append(word)
+#             else:
+#                 anagramMap[sortedWord] = [word]
+#         return anagramMap.values()
         
