639fd3a6b1599543fe79e085e9f035b9f85552d6
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 00:06:24 -0400

    Time: 176 ms (16.01%), Space: 14.4 MB (37.72%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -4,12 +4,10 @@ class Solution:
         mini = 1
         maxi = 1
         for n in nums:
-            if n == 0:
-                mini = 1
-                maxi = 1
-                continue
             temp = n * maxi
             maxi = max(n, n*maxi, n*mini)
             mini = min(temp, n, n*mini)
             ans = max(ans, maxi)
         return ans
+    
+# 1 0 2 -1 3 2 -2
