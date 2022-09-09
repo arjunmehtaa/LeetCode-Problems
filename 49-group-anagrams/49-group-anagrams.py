11207b0e1a30f672a9ef5efb27ddc55a211def0a
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 18:07:53 -0400

    Time: 332 ms (5.02%), Space: 20 MB (9.44%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,8 +1,8 @@
 # Let N be average length of word and M words
 
 # Optimal Solution
-# Time Complexity	: O(M * N LOG(N))
-# Space Complexity	: O(M * N LOG(N))
+# Time Complexity	: O(M * N * 26)
+# Space Complexity	: O(M * N * 26)
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -13,15 +13,11 @@ class Solution:
                 count[ord(char) - ord("a")] += 1
             answer[tuple(count)].append(word)
         return answer.values()
-        
-        
-        
-        
-        
-        
+
+    
 # Brute Force Solution
 # Time Complexity	: O(M * N LOG(N))
-# Space Complexity	: O(M * N LOG(N))
+# Space Complexity	: O(M * N)
 
 # class Solution:
 #     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
