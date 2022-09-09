f41135a2e4b58cbdb3f33c24012d4e6623622fe6
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 12:32:39 -0400

    Time: 120 ms (70.26%), Space: 24.2 MB (13.88%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -32,9 +32,11 @@ class Solution:
         head1 = head
         head2 = prev
         while head2:
-            next = head1.next
+            temp1 = head1.next
+            temp2 = head2.next
             head1.next = head2
-            head1 = head2
-            head2 = next
+            head2.next = temp1
+            head1 = temp1
+            head2 = temp2
         return head
     
