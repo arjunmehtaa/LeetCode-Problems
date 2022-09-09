f7edd872d07a3b88b9d2ad18592ddf0daf7b3b45
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 18:37:57 -0400

    Time: 683 ms (83.57%), Space: 13.9 MB (58.89%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -11,7 +11,6 @@ class Solution:
                     answer = s[l : r + 1]
                 l -= 1
                 r += 1
-        for i in range(len(s)):
             l = i
             r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
