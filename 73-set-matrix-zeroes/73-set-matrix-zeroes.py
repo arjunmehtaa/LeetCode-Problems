0a969f504486515d93ec12fa9d25d3cda201d2f1
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 14:13:50 -0400

    Time: 134 ms (93.04%), Space: 14.8 MB (16.61%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,30 +1,65 @@
+# Brute Force Solution
+# Time Complexity	: O(M*N)
+# Space Complexity	: O(1)
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
-        firstCol = False
+        totalRows = len(matrix)
+        totalCols = len(matrix[0])
+        firstRow = False
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if matrix[r][c] == 0:
-                    matrix[r][0] = 0
-                    if c > 0:
-                        matrix[0][c] = 0
+        for i in range(totalRows):
+            for j in range(totalCols):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
                     else:
-                        firstCol = True
-                        
-        for r in range(1, ROWS):
-            for c in range(1, COLS):
-                if matrix[r][0] == 0 or matrix[0][c] == 0:
-                    matrix[r][c] = 0
-        
+                        firstRow = True
+                    
+        for i in range(1, totalRows):
+            for j in range(1, totalCols):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+                    
         if matrix[0][0] == 0:
-            for c in range(COLS):
-                matrix[0][c] = 0
+            for i in range(totalRows):
+                matrix[i][0] = 0
                 
-        if firstCol:
-            for r in range(ROWS):
-                matrix[r][0] = 0
+        if firstRow:
+            for j in range(totalCols):
+                matrix[0][j] = 0
+                
+
+# Brute Force Solution
+# Time Complexity	: O(M*N)
+# Space Complexity	: O(M*N)
+
+# directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+#
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         totalRows = len(matrix)
+#         totalCols = len(matrix[0])
+#         queue = []
+        
+#         for i in range(totalRows):
+#             for j in range(totalCols):
+#                 if matrix[i][j] == 0:
+#                     queue.append([i, j])
+                    
+#         while len(queue) > 0:
+#             element = queue.pop(0)
+#             r, c = element[0], element[1]
+#             for direction in directions:
+#                 nextRow = r + direction[0]
+#                 nextCol = c + direction[1]
+#                 while nextRow >= 0 and nextCol >= 0 and nextRow < totalRows and nextCol < totalCols:
+#                     matrix[nextRow][nextCol] = 0
+#                     nextRow += direction[0]
+#                     nextCol += direction[1]
+        
+#         return matrix
