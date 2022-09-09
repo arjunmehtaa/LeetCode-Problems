63e0b64366bb6410c3bd157d6b15284220cb4b79
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 14:04:17 -0400

    Time: 278 ms (13.31%), Space: 14.8 MB (54.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,4 +1,6 @@
-directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+# Brute Force Solution
+# Time Complexity	: O(M*N)
+# Space Complexity	: O(1)
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
@@ -7,22 +9,57 @@ class Solution:
         """
         totalRows = len(matrix)
         totalCols = len(matrix[0])
-        queue = []
+        firstRow = False
         
         for i in range(totalRows):
             for j in range(totalCols):
                 if matrix[i][j] == 0:
-                    queue.append([i, j])
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        firstRow = True
                     
-        while len(queue) > 0:
-            element = queue.pop(0)
-            r, c = element[0], element[1]
-            for direction in directions:
-                nextRow = r + direction[0]
-                nextCol = c + direction[1]
-                while nextRow >= 0 and nextCol >= 0 and nextRow < totalRows and nextCol < totalCols:
-                    matrix[nextRow][nextCol] = 0
-                    nextRow += direction[0]
-                    nextCol += direction[1]
+        for i in range(1, totalRows):
+            for j in range(1, totalCols):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+                    
+        if matrix[0][0] == 0:
+            for i in range(totalRows):
+                matrix[i][0] = 0
+                
+        if firstRow:
+            for j in range(totalCols):
+                matrix[0][j] = 0
+                
+
+# Brute Force Solution
+# Time Complexity	: O(M*N)
+# Space Complexity	: O(M*N)
+
+# directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+#
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         totalRows = len(matrix)
+#         totalCols = len(matrix[0])
+#         queue = []
         
-        return matrix
+#         for i in range(totalRows):
+#             for j in range(totalCols):
+#                 if matrix[i][j] == 0:
+#                     queue.append([i, j])
+                    
+#         while len(queue) > 0:
+#             element = queue.pop(0)
+#             r, c = element[0], element[1]
+#             for direction in directions:
+#                 nextRow = r + direction[0]
+#                 nextCol = c + direction[1]
+#                 while nextRow >= 0 and nextCol >= 0 and nextRow < totalRows and nextCol < totalCols:
+#                     matrix[nextRow][nextCol] = 0
+#                     nextRow += direction[0]
+#                     nextCol += direction[1]
+        
+#         return matrix
