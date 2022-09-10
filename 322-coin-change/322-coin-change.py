c22fa9d8ec1890eeff64a671fe72c84813afbc1c
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-10 14:08:14 -0400

    Time: 3368 ms (16.05%), Space: 14 MB (97.00%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,18 +1,31 @@
+# Bottom - Up 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        return dfs(coins, amount, memo)
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1 
         
-def dfs(coins, amountLeft, memo):
-    if amountLeft < 0:
-        return -1
-    if amountLeft == 0:
-        return 0
-    if amountLeft not in memo:
-        answer = inf
-        for coin in coins:
-            value = dfs(coins, amountLeft - coin, memo)
-            if value > -1 and value < answer:
-                answer = value
-        memo[amountLeft] = 1 + answer if answer != inf else -1
-    return memo[amountLeft]
+        
+# Top - Down Memoization
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         memo = {}
+#         return dfs(coins, amount, memo)
+        
+# def dfs(coins, amountLeft, memo):
+#     if amountLeft < 0:
+#         return -1
+#     if amountLeft == 0:
+#         return 0
+#     if amountLeft not in memo:
+#         answer = inf
+#         for coin in coins:
+#             value = dfs(coins, amountLeft - coin, memo)
+#             if value > -1 and value < answer:
+#                 answer = value
+#         memo[amountLeft] = 1 + answer if answer != inf else -1
+#     return memo[amountLeft]
