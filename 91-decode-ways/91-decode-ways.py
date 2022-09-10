63f2118d6b50f880454dd47c6675287cfb3ce7ea
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-10 17:55:33 -0400

    Time: 63 ms (23.09%), Space: 14 MB (46.52%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,22 +1,27 @@
+# Time Complexity	: O(N)
+# Space Complexity	: O(N)
+
 class Solution:
     def numDecodings(self, s: str) -> int:
         memo = {}
-        return dfs(s, memo, 0)
+        return dfs(s, 0, memo)
         
-def dfs(s, memo, i):
+def dfs(s, i, memo):
     if int(s[0]) == 0:
         return 0
-    if len(s) == 1 and int(s) > 0:
+    if (len(s) == 1 and int(s[:1]) >= 1):
         return 1
-    elif len(s) == 2 and int(s) <= 26:
-        return 1 + dfs(s[1], memo, i + 1)
-    if i not in memo:
-        val1 = int(s[:1])
-        val2 = int(s[:2])
+    if (len(s) == 2 and int(s[:2]) <= 26):
+        return 1 + dfs(s[1:], i + 1, memo)
+    if i not in memo:	
+        value1 = int(s[:1])
+        value2 = int(s[:2])
         one, two = 0, 0
-        if val1 >= 1:
-            one = dfs(s[1:], memo, i + 1)
-        if val2 <= 26:
-            two = dfs(s[2:], memo, i + 2)
+        if value1 >= 1:
+            one = dfs(s[1:], i + 1, memo)
+        if value2 <= 26:
+            two = dfs(s[2:], i + 2, memo)
         memo[i] = one + two
     return memo[i]
+        
+        
