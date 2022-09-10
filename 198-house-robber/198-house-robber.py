43e908231bbf763757e88341a4fa8c9ef8f7ec33
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-10 17:28:39 -0400

    Time: 52 ms (45.32%), Space: 13.8 MB (97.31%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,18 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
-        memo = {}
-        return max(dfs(nums, 0, memo), dfs(nums, 1, memo))
+        rob1 = 0
+        rob2 = 0
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if len(nums) <= 2:
+#             return max(nums)
+#         memo = {}
+#         return max(dfs(nums, 0, memo), dfs(nums, 1, memo))
         
-def dfs(nums, index, memo):
-    if index == len(nums) - 1 or index == len(nums) - 2:
-        return nums[index]
-    if index not in memo:
-        answer = 0
-        if index + 2 < len(nums):
-            answer = max(answer, dfs(nums, index + 2, memo))
-        if index + 3 < len(nums):
-            answer = max(answer, dfs(nums, index + 3, memo))
-        memo[index] = answer
-    return nums[index] + memo[index]
+# def dfs(nums, index, memo):
+#     if index == len(nums) - 1 or index == len(nums) - 2:
+#         return nums[index]
+#     if index not in memo:
+#         answer = 0
+#         if index + 2 < len(nums):
+#             answer = max(answer, dfs(nums, index + 2, memo))
+#         if index + 3 < len(nums):
+#             answer = max(answer, dfs(nums, index + 3, memo))
+#         memo[index] = answer
+#     return nums[index] + memo[index]
