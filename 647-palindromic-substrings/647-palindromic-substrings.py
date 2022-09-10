ba8440cb931cfe4776f323e6ef891fb1ae4257cc
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 20:57:12 -0400

    Time: 271 ms (37.82%), Space: 13.9 MB (75.95%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -16,3 +16,33 @@ def countPalindromes(s, l, r):
         l -= 1
         r += 1
     return answer
+
+# ENCODE AND DECODE STRINGS SOLUTION
+
+# class Solution:
+#     """
+#     @param: strs: a list of strings
+#     @return: encodes a list of strings to a single string.
+#     """
+#     def encode(self, strs):
+#         # write your code here
+#         result = ""
+#         for s in strs:
+#             result += str(len(s)) + "#" + s
+#         return result
+#     """
+#     @param: str: A string
+#     @return: dcodes a single string to a list of strings
+#     """
+#     def decode(self, str):
+#         # write your code here
+#         result = []
+#         i = 0
+#         while i < len(str):
+#             j = i
+#             while str[j] != "#":
+#                 j += 1
+#             length = int(str[i:j])
+#             result.append(str[j + 1: j + 1 + length])
+#             i = j + 1 + length
+#         return result
