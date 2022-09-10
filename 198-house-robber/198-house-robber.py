7c1aefa30c183dfc1b45baf397ed8b634f5983b7
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-10 16:39:01 -0400

    Time: 68 ms (9.76%), Space: 14 MB (19.80%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,18 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1 = 0
-        rob2 = 0
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
-
-# Alternate Solution
-# class Solution:
-#     def rob(self, nums: List[int]) -> int:
-#         for i in range(2, len(nums)):
-#             nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
-#             if nums[i - 2] > nums[i - 1]:
-#                 nums[i - 1] = nums[i - 2]
-#         return max(nums[len(nums) - 2], nums[len(nums) - 1])
+        if len(nums) <= 2:
+            return max(nums)
+        memo = {}
+        return max(dfs(nums, 0, memo), dfs(nums, 1, memo))
+        
+def dfs(nums, index, memo):
+    if index == len(nums) - 1 or index == len(nums) - 2:
+        return nums[index]
+    if index not in memo:
+        answer = 0
+        if index + 2 < len(nums):
+            answer = max(answer, dfs(nums, index + 2, memo))
+        if index + 3 < len(nums):
+            answer = max(answer, dfs(nums, index + 3, memo))
+        memo[index] = answer
+    return nums[index] + memo[index]
