572bf4336e278f66fb9a7ada33b08f0f30761c79
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-09 22:02:11 -0400

    Time: 83 ms (17.60%), Space: 16.2 MB (74.20%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -4,19 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Time Complexity   : O(N)
-# Space Complexity  : O(N)
-
-def traverseTree(node, level):
-    if node == None:
-        return level
-    level += 1
-    left = traverseTree(node.left, level)
-    right = traverseTree(node.right, level)
-    return max(left, right)
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        return traverseTree(root, 0)
+        if not root:
+            return 0
+        return traverse(root)
         
+def traverse(node):
+    if node == None:
+        return 1
+    left, right = 0, 0
+    if node.left:
+        left = traverse(node.left)
+    if node.right:
+        right = traverse(node.right)
+    return 1 + max(left, right)
