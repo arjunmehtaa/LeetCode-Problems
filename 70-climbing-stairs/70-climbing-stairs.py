ba00676c3717c8c407275bc7247454fbccacf019
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-12 00:37:13 -0400

    Time: 47 ms (50.96%), Space: 13.9 MB (57.16%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo= {}
-        return dfs(n, memo)
-    
-def dfs(n, memo):
-    if n <= 2:
-        return n
-    if n not in memo:
-        memo[n] = dfs(n-1, memo) + dfs(n-2, memo)
-    return memo[n]
+        if n <= 2:
+            return n
+        one = 2
+        two = 1
+        for i in range(n - 3, -1, -1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
