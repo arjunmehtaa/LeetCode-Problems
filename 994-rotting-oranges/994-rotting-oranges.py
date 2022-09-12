dc10f7b76c005b3d0b1407ddbf4d96cff18c6d16
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-11 21:37:45 -0400

    Time: 103 ms (24.31%), Space: 13.9 MB (91.53%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,67 +1,34 @@
-# Time Complexity   : O(N)
-# Space Complexity  : O(N)
-# where N = A x B for an A x B matrix (N is number of total elements in the grid)
-
 directions = [[1,0], [-1,0], [0,1], [0,-1]]
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0
         mins = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        fresh = 0
         queue = []
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 2:
                     queue.append([i, j])
                 elif grid[i][j] == 1:
                     fresh += 1
         rotten = len(queue)
-        new_rotten = 0
-        while len(queue) > 0:            
+        while len(queue) > 0:
             if rotten == 0:
                 mins += 1
                 rotten = len(queue)
             rotten -= 1
-            pos = queue.pop(0)
-            row = pos[0]
-            col = pos[1]
+            orange = queue.pop(0)
+            row = orange[0]
+            col = orange[1]
             for direction in directions:
-                next_row = row + direction[0]
-                next_col = col + direction[1]
-                if next_row < 0 or next_col < 0 or next_row >= len(grid) or next_col >= len(grid[0]):
+                nextRow = row + direction[0]
+                nextCol = col + direction[1]
+                if nextRow < 0 or nextCol < 0 or nextRow >= rows or nextCol >= cols:
                     continue
-                if grid[next_row][next_col] == 1:
-                    grid[next_row][next_col] = 2
-                    queue.append([next_row, next_col])
+                if grid[nextRow][nextCol] == 1:
+                    grid[nextRow][nextCol] = 2
+                    queue.append([nextRow, nextCol])
                     fresh -= 1
-        if fresh > 0:
-            return -1
-        return mins
-    
-# Walls and Gates Solution (with DFS)
-
-# Time Complexity   : O(N)
-# Space Complexity  : O(N)
-# where N = A x B for an A x B matrix (N is number of total elements in the grid)
-
-# directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
-# def dfs(grid, steps, i, j):
-#     if i<0 or j<0 or i>=len(grid) or j>=len(grid[0])
-#         return
-#     if grid[i][j] == -1:
-#         return
-#     steps += 1
-#     if grid[i][j] > steps:
-#         grid[i][j] = steps
-#     for direction in directions:
-#         row = i + direction[0]
-#         col = j + direction[1]
-#         dfs(grid, steps, row, col)
-
-# class Solution:
-#     def wallsGates(self, grid: List[List[int]]) -> int:
-#         for i in range(0, len(grid)):
-#             for j in range(1 len(grid[0])):
-#                 if grid[i][j] == 0:
-#                     dfs(grid, 1, i, j)
+        return mins if fresh == 0 else -1
