19328b285c0e69e04755093db8ae1697ccb5421b
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-12 00:49:26 -0400

    Time: 448 ms (12.09%), Space: 31.8 MB (34.30%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,49 +1,37 @@
-# Let L be the length of the word to be inserted/searched
-# Time Complexity   : O(L) (for methods)
-# Space Complexity  : O(N) (generally), O(L) (for methods)
-
 class TrieNode:
-    def __init__(self):
-        self. isEnd = False
-        self.keys = {}
+     def __init__(self):
+        self.children = {}
+        self.isEnd = False
 
 class Trie:
+
     def __init__(self):
         self.root = TrieNode()
-        
-    def insert(self, word, node = None):
-        if node == None:
-            node = self.root
-        if len(word) == 0:
-            node.isEnd = True
-            return
-        elif word[0] not in node.keys:
-            node.keys[word[0]] = TrieNode()
-            self.insert(word[1:], node.keys[word[0]])
-        else:
-            self.insert(word[1:], node.keys[word[0]])
+   
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
 
-    def search(self, word, node = None):
-        if node == None:
-            node = self.root
-        if len(word) == 0 and node.isEnd:
-            return True
-        elif len(word) == 0:
-            return False
-        elif word[0] not in node.keys:
-            return False
-        else:
-            return self.search(word[1:], node.keys[word[0]])
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isEnd
 
-    def startsWith(self, prefix, node = None):
-        if node == None:
-            node = self.root
-        if len(prefix) == 0:
-            return True
-        elif prefix[0] not in node.keys:
-            return False
-        else:
-            return self.startsWith(prefix[1:], node.keys[prefix[0]])
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
