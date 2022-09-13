52e05c0f5d9d54d68dfe8214d0fc26c7d73d8051
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-13 19:54:45 -0400

    Time: 867 ms (5.05%), Space: 14.7 MB (49.29%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -3,5 +3,7 @@ class Solution:
         maxDigit = 0
         for char in n:
             maxDigit = max(maxDigit, int(char))
+            if maxDigit == 9:
+                break
         return maxDigit
             
