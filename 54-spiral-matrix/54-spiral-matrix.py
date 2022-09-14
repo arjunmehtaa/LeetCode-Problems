4a09659d615e90d7a9f8ac0f0c5a1930a46900d2
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-13 21:54:18 -0400

    Time: 42 ms (69.35%), Space: 14.1 MB (33.59%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -20,4 +20,59 @@ class Solution:
                 answer.append(matrix[i][left])
             left += 1
         return answer[:total]
-                
+    
+# Brute Force Solution
+# directions = [[0,1], [1,0], [0,-1], [-1,0]]
+
+# class Solution:
+#     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+#         total = len(matrix)*len(matrix[0]) - 1
+#         ans = [matrix[0][0]]
+#         row, col = 0, 0
+#         seen = set()
+#         seen.add((0, 0))
+#         while total > 0:
+#             for direction in directions:
+#                 row += direction[0]
+#                 col += direction[1]
+#                 while (row >= 0 
+#                        and col >= 0 
+#                        and row < len(matrix) 
+#                        and col < len(matrix[0]) 
+#                        and (row, col) not in seen):
+#                     ans.append(matrix[row][col])
+#                     total -= 1
+#                     seen.add((row, col))
+#                     row += direction[0]
+#                     col += direction[1]
+#                 row -= direction[0]
+#                 col -= direction[1]
+#         return ans
+                    
+        
+# # BFS in 2D Array
+
+# # Time Complexity   : O(N)
+# # Space Complexity  : O(N)
+
+# # Note: If values are allowed to be duplicate, seen functionality would have to change
+# # to a an array of boolean values, where we can check if seen[i][j] is true
+
+# class Solution:
+#     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+#         seen = set()
+#         ans = []
+#         queue = [[0,0]]
+#         while len(queue) > 0:
+#             position = queue.pop(0)
+#             i = position[0]
+#             j = position[1]
+#             if i < 0 or j < 0 or i >= len(matrix) or j >= len(matrix[0]) or matrix[i][j] in seen:
+#                 continue
+#             ans.append(matrix[i][j])
+#             seen.add(matrix[i][j])
+#             queue.append([i-1, j])
+#             queue.append([i, j+1])
+#             queue.append([i+1, j])
+#             queue.append([i, j-1])
+#         print(ans)
