a87a0eb954741fd8a599ec7579eeb426953da83d
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-16 19:29:11 -0400

    Time: 116 ms (48.55%), Space: 14.1 MB (13.81%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,37 +1,11 @@
-# Optimal Solution
-#
-# Time Complexity   :   O(N)
-# Space Complexity  :   O(N)
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-        answer = 0
-        data = {}
+        map = {}
         a = 0
-        for b in range(0, len(s)):
-            if s[b] in data and data[s[b]] >= a:
-                a = data[s[b]] + 1
-            data[s[b]] = b
-            answer = max(answer, b - a + 1)
-        return answer           
-
-# Brute Force Solution
-#
-# Time Complexity   :   O(N^2)
-# Space Complexity  :   O(N)
-#
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         if len(s) <= 1:
-#             return len(s)
-#         answer = 0
-#         for i in range(0, len(s)):
-#             data = set()
-#             j = i
-#             while j < len(s) and s[j] not in data:
-#                 data.add(s[j])
-#                 j += 1
-#                 answer = max(answer, len(data))
-#         return answer
+        res = 0
+        for b in range(len(s)):
+            if s[b] in map and map[s[b]] >= a:
+                a = map[s[b]] + 1
+            map[s[b]] = b
+            res = max(res, b - a + 1)
+        return res
