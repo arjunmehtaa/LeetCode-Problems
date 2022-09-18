30713bb2b180cd743d5490fe77de6ce9b1c7828b
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-18 16:30:00 -0400

    Time: 152 ms (14.51%), Space: 13.9 MB (86.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -12,10 +12,8 @@ class Solution:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
             sum = val1 + val2 + carry
-            if l1: 
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
             carry = 0
             if sum > 9:
                 sumStr = str(sum)
