4ac8babb2ccbd5a2dbc123a0abdd77a646a9dc10
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-09-17 21:53:20 -0400

    Time: 195 ms (52.58%), Space: 15.2 MB (35.89%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,27 +6,22 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-        queue = [root]
-        while len(queue) > 0:
-            node = queue.pop(0)
-            if areTreesIdentical(node, subRoot):
-                return True
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        return False
+        if isSame(root, subRoot):
+            return True
+        left, right = False, False
+        if root.left:
+            left = self.isSubtree(root.left, subRoot)
+        if root.right:
+            right = self.isSubtree(root.right, subRoot)
+        return left or right
         
         
-def areTreesIdentical(root, subRoot):
+def isSame(root, subRoot):
     if not root and not subRoot:
         return True
-    if not root or not subRoot:
+    elif not root or not subRoot:
         return False
-    if root.val != subRoot.val:
+    elif root.val == subRoot.val:
+        return isSame(root.left, subRoot.left) and isSame(root.right, subRoot.right)
+    else:
         return False
-    if not areTreesIdentical(root.left, subRoot.left) or not areTreesIdentical(root.right, subRoot.right):
-        return False
-    return True
