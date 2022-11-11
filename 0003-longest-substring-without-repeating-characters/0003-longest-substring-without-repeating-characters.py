c314968f7a3030eeea12a2837b96fc93ab924288
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2022-11-10 20:09:43 -0500

    Time: 53 ms (98.60%), Space: 13.9 MB (99.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,37 @@
+# Optimal Solution
+#
+# Time Complexity   :   O(N)
+# Space Complexity  :   O(N)
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        map = {}
+        if len(s) <= 1:
+            return len(s)
+        answer = 0
+        data = {}
         a = 0
-        b = 0
-        ans = 0
-        while b < len(s):
-            if s[b] in map and map[s[b]] >= a:
-                a = map[s[b]] + 1
-            map[s[b]] = b
-            ans = max(ans, b - a + 1)
-            b += 1
-        return ans
-            
-        
+        for b in range(0, len(s)):
+            if s[b] in data and data[s[b]] >= a:
+                a = data[s[b]] + 1
+            data[s[b]] = b
+            answer = max(answer, b - a + 1)
+        return answer           
+
+# Brute Force Solution
+#
+# Time Complexity   :   O(N^2)
+# Space Complexity  :   O(N)
+#
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) <= 1:
+#             return len(s)
+#         answer = 0
+#         for i in range(0, len(s)):
+#             data = set()
+#             j = i
+#             while j < len(s) and s[j] not in data:
+#                 data.add(s[j])
+#                 j += 1
+#                 answer = max(answer, len(data))
+#         return answer
