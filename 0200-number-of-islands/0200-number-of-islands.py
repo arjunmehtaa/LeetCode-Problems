ef5b36052508e8bbb488fad34412c6ad39b07706
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-01-19 02:11:52 -0500

    Time: 341 ms (64.82%), Space: 16.1 MB (97.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,32 @@
-directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+# Time Complexity   : O(M X N) 		(It is O(2N) because even though we might end up traversing through 
+#					                the entire grid, we would never encounter that island again)
+# Space Complexity  : O(max(M, N))	(where matrix is N X M)
+
+directions = [[-1,0], [0,1], [1,0], [0,-1]]
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ans = 0
-        n, m = len(grid), len(grid[0])
-        for i in range(n):
-            for j in range(m):
+        if len(grid) == 0:
+            return False
+        islands = 0
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[0])):
                 if grid[i][j] == "1":
-                    ans += 1
-                    queue = [[i, j]]
-                    while len(queue):
-                        ele = queue.pop(0)
-                        for direction in directions:
-                            r = ele[0] + direction[0]
-                            c = ele[1] + direction[1]
-                            if r < 0 or c < 0 or r >= n or c >= m:
+                    islands += 1
+                    grid[i][j] = "0"
+                    queue = [[i,j]]
+                    while len(queue) > 0:
+                        pos = queue.pop(0)
+                        row = pos[0]
+                        col = pos[1]
+                        for k in range(0, len(directions)):
+                            current_dir = directions[k]
+                            next_row = row + current_dir[0]
+                            next_col = col + current_dir[1]
+                            if next_row < 0 or next_col < 0 or next_row >= len(grid) or next_col >= len(grid[0]):
                                 continue
-                            if grid[r][c] == "1":
-                                grid[r][c] = "0"
-                                queue.append([r, c])
-        return ans
-                        
+                            if grid[next_row][next_col] == "1":
+                                queue.append([next_row, next_col])
+                                grid[next_row][next_col] = "0"
+        return islands                     
+    
