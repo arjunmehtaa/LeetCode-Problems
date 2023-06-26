a59c7362b64d62ef1b05f84d5e00cc0a2d927839
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-06-25 17:52:33 -0700

    Time: 117 ms (62.78%), Space: 19.9 MB (86.99%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,7 +1,3 @@
-# K is the number of sorted lists
-# Time Complexity	: O(N*LOG(K))
-# Space Complexity	: O(1)             NOT SURE
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -15,29 +11,22 @@ class Solution:
             mergedLists = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i+1] if i+1 < len(lists) else None
-                mergedLists.append(merge(l1, l2))
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(mergeLists(l1, l2))
             lists = mergedLists
         return lists[0]
-                                
-def merge(l1, l2):
-    final = ListNode()
-    head = final
+                
+        
+def mergeLists(l1, l2):
+    ans = ListNode()
+    ref = ans
     while l1 and l2:
         if l1.val < l2.val:
-            final.next = l1
+            ref.next = l1
             l1 = l1.next
         else:
-            final.next = l2
+            ref.next = l2
             l2 = l2.next
-        final = final.next
-    if l1:
-        final.next = l1
-    if l2:
-        final.next = l2
-    return head.next
-                                   
-                        
-                                   
-                                   
-        
+        ref = ref.next
+    ref.next = l1 or l2
+    return ans.next
