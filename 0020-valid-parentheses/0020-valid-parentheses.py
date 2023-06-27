834ba7593311a634f005d169a326a2fbd9f1dbab
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-06-26 20:05:36 -0700

    Time: 45 ms (70.49%), Space: 16.3 MB (90.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,16 @@
-dataMap = {'}':'{', ')':'(', ']':'['}
+mapping = {
+    ")":"(", 
+    "}": "{", 
+    "]": "["
+}
 
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for char in s:
-            if char in dataMap:
-                if len(stack) == 0 or not stack.pop() == dataMap[char]:
+            if char in mapping:
+                if len(stack) == 0 or stack.pop() != mapping[char]:
                     return False
             else:
                 stack.append(char)
         return len(stack) == 0
-        
