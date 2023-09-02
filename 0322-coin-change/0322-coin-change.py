33e2c49e88ac62ece7c34bf6ab404e48aaec6c9f
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-09-02 16:46:20 -0700

    Time: 1418 ms (21.78%), Space: 27.9 MB (22.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,33 +1,21 @@
-# Bottom - Up 
-# Time Complexity	: O(amount * len(coins))
-# Space Complexity	: O(amount)
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
-        for i in range(1, amount + 1):
-            for coin in coins:
-                if i - coin >= 0:
-                    dp[i] = min(dp[i], 1 + dp[i - coin])
-        return dp[amount] if dp[amount] != amount + 1 else -1      
-        
-# Top - Down Memoization
-# class Solution:
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-#         memo = {}
-#         return dfs(coins, amount, memo)
-        
-# def dfs(coins, amountLeft, memo):
-#     if amountLeft < 0:
-#         return -1
-#     if amountLeft == 0:
-#         return 0
-#     if amountLeft not in memo:
-#         answer = inf
-#         for coin in coins:
-#             value = dfs(coins, amountLeft - coin, memo)
-#             if value > -1 and value < answer:
-#                 answer = value
-#         memo[amountLeft] = 1 + answer if answer != inf else -1
-#     return memo[amountLeft]
+        dp = {}
+        for coin in coins:
+            dp[coin] = 1
+        return traverse(amount, dp, coins)
+            
+def traverse(amount, dp, coins):
+    if amount == 0:
+        return 0
+    if amount < 0:
+        return -1
+    if amount not in dp:
+        ans = inf
+        for coin in coins:
+            res = 1 + traverse(amount - coin, dp, coins)
+            if res > 0:
+                ans = min(ans, res)
+        dp[amount] = ans
+    return dp[amount] if dp[amount] != inf else -1
+
