6a7015e0f3cd34293c0ba0a79d85268b4b95dcd2
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-09-04 12:48:35 -0700

    Time: 40 ms (63.92%), Space: 16.3 MB (70.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,28 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = {}
-        n = len(nums)
-        
-        def traverse(index):
-            if index not in dp:
-                if (n - index) == 1:
-                    dp[index] = nums[index]
-                elif (n - index) == 2:
-                    dp[index] = max(nums[index:])
-                else:
-                    dp[index] = nums[index] + dp[index + 2]
-                    if index + 3 < len(nums):
-                        dp[index] = max(dp[index], nums[index] + dp[index + 3])
-            return dp[index]
-        
-        
-        for i in range(len(nums) - 1, -1, -1):
-            traverse(i)
-                        
-        return max(dp.values())
-        
-        
-        
-        
-        
-        
+        one = 0
+        two = 0
+        for n in nums:
+            temp = one
+            one = two
+            two = max(n + temp, two)
+        return two
