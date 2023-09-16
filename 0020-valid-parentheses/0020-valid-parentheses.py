9bcd613a1c4984f5a9137a374bac8969361b98a1
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-09-16 10:52:43 -0700

    Time: 34 ms (85.94%), Space: 16.2 MB (81.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,14 @@
-map = {"{":"}", "[":"]", "(":")"}
+# Time Complexity   : O(N)
+# Space Complexity  : O(N)
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        map = {']':'[', '}':'{', ')':'('}
         stack = []
         for char in s:
-            if char in map:
-                stack.append(map[char])
+            if char in map.values():
+                stack.append(char)
             else:
-                if len(stack) == 0 or char != stack.pop():
+                if len(stack) == 0 or stack.pop() != map[char]:
                     return False
         return len(stack) == 0
