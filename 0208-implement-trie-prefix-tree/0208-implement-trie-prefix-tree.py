9172ca52a46e02f0704d1a8f61176211c1824a95
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-09-16 14:18:29 -0700

    Time: 166 ms (44.89%), Space: 33.8 MB (63.98%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,33 +1,49 @@
-class TrieNode:
+class Node:
+    
     def __init__(self):
         self.children = {}
         self.isEnd = False
-        
+
 class Trie:
-    
+
     def __init__(self):
-        self.root = TrieNode()
+        self.head = Node()
         
-    def insert(self, word):
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.isEnd = True
+    def insert(self, word: str) -> None:
+        i = 0
+        curr = self.head
+        while i < len(word):
+            if word[i] not in curr.children:
+                curr.children[word[i]] = Node()
+            curr = curr.children[word[i]]
+            i += 1
+        curr.isEnd = True
             
-    def search(self, word):
-        node = self.root
-        for char in word:
-            if char not in node.children:
+
+    def search(self, word: str) -> bool:
+        curr = self.head
+        i = 0
+        while i < len(word):
+            if word[i] not in curr.children:
                 return False
-            node = node.children[char]
-        return node.isEnd
-    
-    def startsWith(self, word):
-        node = self.root
-        for char in word:
-            if char not in node.children:
+            curr = curr.children[word[i]]
+            i += 1
+        return curr.isEnd
+            
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.head
+        i = 0
+        while i < len(prefix):
+            if prefix[i] not in curr.children:
                 return False
-            node = node.children[char]
+            curr = curr.children[prefix[i]]
+            i += 1
         return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
