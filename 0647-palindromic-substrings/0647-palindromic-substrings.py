98c5bdb26f472bc2bdb7490e9e3a021454be1a54
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-10-18 11:59:50 -0400

    Time: 88 ms (93.29%), Space: 16.2 MB (86.79%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,48 +1,20 @@
-# Time Complexity	: O(N^2)
-# Space Complexity	: O(1)
-
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        answer = 0
-        for i in range(0, len(s)):
-            answer += countPalindromes(s, i, i)
-            answer += countPalindromes(s, i, i + 1)
-        return answer
-    
-def countPalindromes(s, l, r):
-    answer = 0
-    while l >= 0 and r < len(s) and s[l] == s[r]:
-        answer += 1
-        l -= 1
-        r += 1
-    return answer
-
-# ENCODE AND DECODE STRINGS SOLUTION
-
-# class Solution:
-#     """
-#     @param: strs: a list of strings
-#     @return: encodes a list of strings to a single string.
-#     """
-#     def encode(self, strs):
-#         # write your code here
-#         result = ""
-#         for s in strs:
-#             result += str(len(s)) + "#" + s
-#         return result
-#     """
-#     @param: str: A string
-#     @return: dcodes a single string to a list of strings
-#     """
-#     def decode(self, str):
-#         # write your code here
-#         result = []
-#         i = 0
-#         while i < len(str):
-#             j = i
-#             while str[j] != "#":
-#                 j += 1
-#             length = int(str[i:j])
-#             result.append(str[j + 1: j + 1 + length])
-#             i = j + 1 + length
-#         return result
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            a = i
+            b = i
+            while a >= 0 and b < n and s[a] == s[b]:
+                ans += 1
+                a -= 1
+                b += 1
+        for i in range(n - 1):
+            a = i
+            b = i + 1
+            while a >= 0 and b < n and s[a] == s[b]:
+                ans += 1
+                a -= 1
+                b += 1
+        return ans
+                
