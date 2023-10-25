dde8cfd35b6f788271de29d88f9d10ea3a068b87
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2023-10-25 01:27:01 -0400

    Time: 685 ms (64.16%), Space: 78.5 MB (35.12%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,4 +1,5 @@
 class Node:
+    
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -14,6 +15,19 @@ class LRUCache:
         self.right = Node(0, 0)
         self.left.next = self.right
         self.right.prev = self.left
+        
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.right
+        self.right.prev = node
+        
+    def remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
 
     def get(self, key: int) -> int:
         if key not in self.cache:
@@ -31,21 +45,6 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-        
-        
-    def insert(self, node):
-        prev = self.right.prev
-        prev.next = node
-        node.prev = prev
-        node.next = self.right
-        self.right.prev = node
-        
-    def remove(self, node):
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
-            
 
 
 # Your LRUCache object will be instantiated and called as such:
