df272c069de16f7ec476a5765fe24f24109f0b91
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-01-31 17:36:41 -0500

    Time: 43 ms (98.89%), Space: 24.5 MB (97.27%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,35 +8,35 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        if not head:
+            return None
+        
+        # Find the middle
         slow = head
         fast = head
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-        start = slow.next
+        current = slow.next
+        
+        # Reverse second half
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
         slow.next = None
         
-        prev = None
-        while start:
-            next = start.next
-            start.next = prev
-            prev = start
-            start = next
-            
-        final = ListNode()
-        p = final
-        current = head
-        while current and prev:
-            p.next = current
-            current = current.next
-            p = p.next
-            p.next = prev
-            prev = prev.next
-            p = p.next
-        p.next = current or prev
-        return final.next
-            
-            
-        
-        
-        
+        # Merge the two lists
+        head1 = head
+        head2 = prev
+        while head2:
+            temp1 = head1.next
+            temp2 = head2.next
+            head1.next = head2
+            head2.next = temp1
+            head1 = temp1
+            head2 = temp2
+        return head
+    
