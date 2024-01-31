0bfdd5cc65d0b65522d48917c7558b52d8ee47c8
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-01-31 14:42:29 -0500

    Time: 71 ms (92.24%), Space: 19.4 MB (89.51%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,7 +1,3 @@
-# K is the number of sorted lists
-# Time Complexity	: O(N*LOG(K))
-# Space Complexity	: O(1)             NOT SURE
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,35 +5,29 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
         while len(lists) > 1:
+            n = len(lists)
             mergedLists = []
-            for i in range(0, len(lists), 2):
+            for i in range(0, n, 2):
                 l1 = lists[i]
-                l2 = lists[i+1] if i+1 < len(lists) else None
-                mergedLists.append(merge(l1, l2))
+                l2 = lists[i+1] if i+1 < n else None
+                mergedLists.append(mergeList(l1, l2))
             lists = mergedLists
         return lists[0]
-                                
-def merge(l1, l2):
+        
+        
+def mergeList(l1, l2):
     final = ListNode()
-    head = final
+    current = final
     while l1 and l2:
         if l1.val < l2.val:
-            final.next = l1
+            current.next = l1
             l1 = l1.next
         else:
-            final.next = l2
+            current.next = l2
             l2 = l2.next
-        final = final.next
-    if l1:
-        final.next = l1
-    if l2:
-        final.next = l2
-    return head.next
-                                   
-                        
-                                   
-                                   
-        
+        current = current.next
+    current.next = l1 or l2
+    return final.next
