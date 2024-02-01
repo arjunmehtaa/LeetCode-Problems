87bdb87ff5cf82c9a3442b2f363c37094e195b23
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-01-31 21:40:49 -0500

    Time: 328 ms (75.35%), Space: 16.7 MB (62.90%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,26 @@
-# Time Complexity	: O(N^2)
-# Space Complexity	: O(1)
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        resLen = 0
-        for i in range(len(s)):
-            l = i
-            r = i
-            # odd length
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
-                l -= 1
-                r += 1
-            # even length
-            l = i
-            r = i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
-                l -= 1
-                r += 1
-        return res
-                    
+        maxLen = 0
+        ans = ""
+        
+        for i in range(0, len(s)):
+            a = i
+            b = i
+            while a >= 0 and b < len(s) and s[a] == s[b]:
+                if (b - a + 1) > maxLen:
+                    maxLen = b - a + 1
+                    ans = s[a:b+1]
+                a -= 1
+                b += 1
+            a = i
+            b = i + 1
+            while a >= 0 and b < len(s) and s[a] == s[b]:
+                if (b - a + 1) > maxLen:
+                    maxLen = b - a + 1
+                    ans = s[a:b+1]
+                a -= 1
+                b += 1
+            
                 
+        return ans
+            
