62631edaab8632f328464d208a95c9e44cb7bb51
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-01-31 21:06:18 -0500

    Time: 64 ms (16.06%), Space: 16.9 MB (80.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charMap = {}
-        for char in s:
-            charMap[char] = charMap.get(char, 0) + 1
-        for char in t:
-            if char not in charMap:
+        if len(s) != len(t):
+            return False
+        seen = {}
+        for i in range(len(s)):
+            seen[s[i]] = seen.get(s[i], 0) + 1
+        for i in range(len(t)):
+            if t[i] not in seen:
                 return False
-            charMap[char] -= 1
-            if charMap[char] == 0:
-                del charMap[char]
-        return len(charMap) == 0
+            seen[t[i]] -= 1
+            if seen[t[i]] == 0:
+                del seen[t[i]]
+        return len(seen) == 0
