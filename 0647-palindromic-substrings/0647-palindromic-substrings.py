c72294bd7ced23e8e9a85553ce781bc048d4b23f
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-01-31 21:44:23 -0500

    Time: 88 ms (61.42%), Space: 16.5 MB (78.64%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,17 +1,48 @@
+# Time Complexity	: O(N^2)
+# Space Complexity	: O(1)
+
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        ans = 0
-        for i in range(len(s)):
-            a = i
-            b = i
-            while a >= 0 and b < len(s) and s[a] == s[b]:
-                ans += 1
-                a -= 1
-                b += 1
-            a = i
-            b = i + 1
-            while a >= 0 and b < len(s) and s[a] == s[b]:
-                ans += 1
-                a -= 1
-                b += 1
-        return ans
+        answer = 0
+        for i in range(0, len(s)):
+            answer += countPalindromes(s, i, i)
+            answer += countPalindromes(s, i, i + 1)
+        return answer
+    
+def countPalindromes(s, l, r):
+    answer = 0
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        answer += 1
+        l -= 1
+        r += 1
+    return answer
+
+# ENCODE AND DECODE STRINGS SOLUTION
+
+# class Solution:
+#     """
+#     @param: strs: a list of strings
+#     @return: encodes a list of strings to a single string.
+#     """
+#     def encode(self, strs):
+#         # write your code here
+#         result = ""
+#         for s in strs:
+#             result += str(len(s)) + "#" + s
+#         return result
+#     """
+#     @param: str: A string
+#     @return: dcodes a single string to a list of strings
+#     """
+#     def decode(self, str):
+#         # write your code here
+#         result = []
+#         i = 0
+#         while i < len(str):
+#             j = i
+#             while str[j] != "#":
+#                 j += 1
+#             length = int(str[i:j])
+#             result.append(str[j + 1: j + 1 + length])
+#             i = j + 1 + length
+#         return result
