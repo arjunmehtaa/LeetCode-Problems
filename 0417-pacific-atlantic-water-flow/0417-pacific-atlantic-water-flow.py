a1bd992bbe9ad8ec5309cee802a029465f5617a1
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-17 10:04:19 -0400

    Time: 219 ms (39.84%), Space: 18.1 MB (55.81%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,36 +1,39 @@
-directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+# Let heights be an M x N matrix
+# Time Complexity	: O(M x N)
+# Space Complexity	: O(M x N)
+
+directions = [[-1,0], [1,0], [0,-1], [0,1]]
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        
         rows = len(heights)
         cols = len(heights[0])
-        ans = []
         pac = set()
         atl = set()
+        result = []
         
-        def traverse(i, j, visit):
-            if i < 0 or j < 0 or i >= rows or j >= cols or (i, j) in visit:
+        def dfs(r, c, visit, prevHeight):
+            if (r < 0 or c < 0 or r >= rows or c >= cols or (r,c) in visit or 
+                heights[r][c] < prevHeight):
                 return
-            visit.add((i, j))
-            for d in directions:
-                r = i + d[0]
-                c = j + d[1]
-                if r >= 0 and c >= 0 and r < rows and c < cols and (r, c) not in visit and  heights[r][c] >= heights[i][j]:
-                    traverse(r, c, visit)
-                    
-        for i in range(rows):
-            traverse(i, 0, pac)
-            traverse(i, cols - 1, atl)
+            visit.add((r, c))
+            for direction in directions:
+                next_row = r + direction[0]
+                next_col = c + direction[1]
+                dfs(next_row, next_col, visit, heights[r][c])
+        
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
             
-        for j in range(cols):
-            traverse(0, j, pac)
-            traverse(rows - 1, j, atl)
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
             
-        for (a, b) in pac:
-            if (a, b) in atl and [a, b] not in ans:
-                ans.append([a, b])
-                
-        return ans
-                    
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    result.append([r, c])
+        
+        return result
         
