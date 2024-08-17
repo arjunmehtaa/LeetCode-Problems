7bade7b00c4aba646c0db9ffe23f414dbca92922
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-17 12:39:01 -0400

    Time: 32 ms (74.55%), Space: 16.4 MB (57.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,25 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one = 1
-        two = 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
-        
-# class Solution:
-#     def climbStairs(self, n: int) -> int:
-#         memo = {}
-        
-#         def traverse(index):
-#             if index == 0:
-#                 return 1
-#             if index < 0:
-#                 return 0
-#             if index not in memo:
-#                 memo[index] = traverse(index - 1) + traverse(index - 2)
-#             return memo[index]
-        
-#         return traverse(n)
-        
+        if n < 3:
+            return n
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = two
+            two = one + two
+            one = temp
+        return two
+            
+            
