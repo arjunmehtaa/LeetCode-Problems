3b30b9e823f4974220e20524f6ee2b7e8af42148
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-17 22:56:40 -0400

    Time: 34 ms (71.67%), Space: 16.5 MB (66.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0] * n
-        one, two = 0, 0
-        for i in range(n - 1, -1, -1):
-            dp[i] = max(nums[i] + two, one)
-            two = one
-            one = dp[i]
-        return dp[0]
+        rob1 = 0
+        rob2 = 0
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if len(nums) <= 2:
+#             return max(nums)
+#         memo = {}
+#         return max(dfs(nums, 0, memo), dfs(nums, 1, memo))
+        
+# def dfs(nums, index, memo):
+#     if index == len(nums) - 1 or index == len(nums) - 2:
+#         return nums[index]
+#     if index not in memo:
+#         answer = 0
+#         if index + 2 < len(nums):
+#             answer = max(answer, dfs(nums, index + 2, memo))
+#         if index + 3 < len(nums):
+#             answer = max(answer, dfs(nums, index + 3, memo))
+#         memo[index] = answer
+#     return nums[index] + memo[index]
