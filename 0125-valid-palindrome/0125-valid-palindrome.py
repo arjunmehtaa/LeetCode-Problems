a378b3677017c2029deacd646a4bc9bc78c20379
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-18 17:04:30 -0400

    Time: 44 ms (61.98%), Space: 17.9 MB (26.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,6 +2,7 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^A-Za-z0-9]+', '', s)
+        s = re.sub("[^A-Za-z0-9]+", "", s)
         s = s.lower()
         return s == s[::-1]
+        
