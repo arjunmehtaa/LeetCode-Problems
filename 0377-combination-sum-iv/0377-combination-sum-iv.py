6be664eea54479ae42edb4ececf3cd1b8c4f53ce
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-17 22:18:04 -0400

    Time: 39 ms (66.00%), Space: 16.6 MB (19.75%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,42 +1,18 @@
-# Bottom-up Dynamic Solution
-#
-# Let N be the target value and M be the size of nums array
-# Time Complexity	: O(N*M)
-# Space Complexity	: O(N)
-
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = {0 : 1}
-        for total in range(1, target + 1):
-            dp[total] = 0
-            for n in nums:
-                dp[total] += dp.get(total - n, 0)
-        return dp[target]
+        memo = {}
         
-# DFS + Memoization Solution
-#
-# Let N be the target value and M be the size of nums array
-# Time Complexity	: O(N*M)
-# Space Complexity	: O(N*M)
-#
-# class Solution:
-#     def combinationSum4(self, nums: List[int], target: int) -> int:
-#         res = 0
-#         ans = 0
-#         memo = {}
-#         for num in nums:
-#             res += dfs(nums, num, ans, target, memo)
-#         return res
+        def traverse(t):
+            if t == 0:
+                return 1
+            if t < 0:
+                return 0
+            if t not in memo:
+                possible = 0
+                for n in nums:
+                    possible += traverse(t - n)
+                memo[t] = possible
+            return memo[t]
         
-# def dfs(nums, num, ans, target, memo):
-#     ans += num
-#     if ans == target:
-#         return 1
-#     elif ans > target:
-#         return 0
-#     if ans not in memo:
-#         count = 0
-#         for n in nums:
-#             count += dfs(nums, n, ans, target, memo)
-#         memo[ans] = count
-#     return memo[ans]
+        return traverse(target)
+            
