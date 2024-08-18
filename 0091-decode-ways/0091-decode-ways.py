3912694119d3d9ada3b02e1dbe77237ffa7b8148
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-18 00:33:14 -0400

    Time: 35 ms (65.07%), Space: 16.6 MB (50.64%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,46 +1,45 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         memo = {}
-        return traverse(s, 0, memo)
-    
-def traverse(s, index, memo):
-    if index == len(s):
-        return 1
-    if index > len(s):
-        return 0
-    if index not in memo:
-        res = 0
-        if int(s[index]) != 0:
-            res += traverse(s, index + 1, memo)
-            if index < len(s) - 1 and int(s[index:index+2]) <= 26:
-                res += traverse(s, index + 2, memo)
-        memo[index] = res
-    return memo[index]
-
-# Time Complexity	: O(N)
-# Space Complexity	: O(N)
-
-# class Solution:
-#     def numDecodings(self, s: str) -> int:
-#         memo = {}
-#         return dfs(s, 0, memo)
+        n = len(s)
+            
+        def traverse(i):
+            if i == n:
+                return 1
+            if i > n:
+                return 0
+            if i not in memo:
+                memo[i] = 0
+                if int(s[i]) > 0:
+                    memo[i] = traverse(i + 1)
+                    if i < n - 1 and int(s[i:i + 2]) <= 26:
+                        memo[i] += traverse(i + 2)
+            return memo[i]
+                                  
+        return traverse(0)
         
-# def dfs(s, i, memo):
-#     if int(s[0]) == 0:
-#         return 0
-#     if (len(s) == 1 and int(s[:1]) >= 1):
-#         return 1
-#     if (len(s) == 2 and int(s[:2]) <= 26):
-#         return 1 + dfs(s[1:], i + 1, memo)
-#     if i not in memo:	
-#         value1 = int(s[:1])
-#         value2 = int(s[:2])
-#         one, two = 0, 0
-#         if value1 >= 1:
-#             one = dfs(s[1:], i + 1, memo)
-#         if value2 <= 26:
-#             two = dfs(s[2:], i + 2, memo)
-#         memo[i] = one + two
-#     return memo[i]
         
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(s)
+        # dp = [1] * (n + 1)
+        # dp[n] = 1
+        # if s[0] == "0" or "00" in s:
+        #     return 0
+        # for i in range(n - 1, -1, -1):
+        #     int_val = int(s[i])
+        #     if (i < n - 1 and (int_val == 1 or (int_val == 2 and int(s[i + 1]) <= 6))):
+        #         if int(s[i + 1]) == 0:
+        #             dp[i] = dp[i + 1]
+        #         else:
+        #             dp[i] = 1 + dp[i + 1]
+        #     elif int(s[i]) > 0:
+        #         dp[i] = dp[i + 1]
+        # return dp[0]
         
