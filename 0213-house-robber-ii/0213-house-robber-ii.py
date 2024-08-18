e809457ba89380a06af940b0a23128130f698ce6
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-17 23:05:05 -0400

    Time: 35 ms (65.20%), Space: 16.6 MB (7.12%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,12 +1,15 @@
+# Time Complexity	: O(N)
+# Space Complexity	: O(1)
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0], stealMoney(nums[1:]), stealMoney(nums[:len(nums) - 1]))
-        
-def stealMoney(nums):
-    one, two = 0, 0
-    for i in range(len(nums) - 1, -1, -1):
-        temp = one
-        one = max(nums[i] + two, one)
-        two = temp
-    return one
+        return max(nums[0], helper(nums[1:]), helper(nums[:len(nums)-1]))
     
+def helper(nums):
+    rob1 = 0
+    rob2 = 0
+    for n in nums:
+        temp = max(rob1 + n, rob2)
+        rob1 = rob2
+        rob2 = temp
+    return rob2
