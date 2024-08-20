88d71ed5a2e5ccf9f8ac74cc2a4d75ab8dcac8dd
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-20 00:56:12 -0400

    Time: 36 ms (86.05%), Space: 18.4 MB (14.96%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,14 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Time Complexity   : O(N)
+# Space Complexity  : O(N)
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return traverseTree(root, -inf, inf)
+
+def traverseTree(node, min, max):
+    if node.val <= min or node.val >= max:
+        return False
+    if node.left:
+        if not traverseTree(node.left, min, node.val):
+            return False
+    if node.right:
+        if not traverseTree(node.right, node.val, max):
+            return False
+    return True
         
-        def checkBST(node, mini, maxi):
-            if not node:
-                return True
-            if node.val <= mini or node.val >= maxi:
-                return False
-            return checkBST(node.left, mini, node.val) and checkBST(node.right, node.val, maxi)
         
-        return checkBST(root, -inf, inf)
