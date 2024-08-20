00c727c0205652e6a83a31aade07ce928fa161ef
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-20 01:57:56 -0400

    Time: 1693 ms (29.29%), Space: 64.3 MB (40.58%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,4 +1,4 @@
-class TrieNode:
+class Node:
     def __init__(self):
         self.children = {}
         self.isEnd = False
@@ -6,32 +6,29 @@ class TrieNode:
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = Node()
 
     def addWord(self, word: str) -> None:
         node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node()
+            node = node.children[c]
         node.isEnd = True
 
     def search(self, word: str) -> bool:
-        
-        def dfs(node, j):
-            for i in range(j, len(word)):
-                if word[i] == ".":
-                    for child in node.children.values():
-                        if dfs(child, i + 1):
-                            return True
-                    return False
-                else:
-                    if word[i] not in node.children:
-                        return False
-                    node = node.children[word[i]]
-            return node.isEnd    
-                
-        return dfs(self.root, 0)
+        return self.compare(self.root, word)
+    
+    def compare(self, node, word):
+        for i in range(len(word)):
+            if word[i] == ".":
+                for child in node.children.values():
+                    if self.compare(child, word[i + 1:]):
+                        return True
+            if word[i] not in node.children:
+                return False
+            node = node.children[word[i]]
+        return node.isEnd
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
