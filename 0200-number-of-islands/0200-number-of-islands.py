6280c9cdcf52ac82374511161785920a6e087b5f
Arjun Mehta <71606157+arjunmehtaa@users.noreply.github.com>
2024-08-21 23:15:19 -0400

    Time: 250 ms (50.14%), Space: 18.9 MB (79.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,23 @@
-
-directions = [[-1,0], [0,1], [1,0], [0,-1]]
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0:
-            return False
-        islands = 0
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
+        rows = len(grid)
+        cols = len(grid[0])
+        ans = 0
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1":
-                    islands += 1
                     grid[i][j] = "0"
-                    queue = [[i,j]]
-                    while len(queue) > 0:
-                        pos = queue.pop(0)
-                        row = pos[0]
-                        col = pos[1]
-                        for k in range(0, len(directions)):
-                            current_dir = directions[k]
-                            next_row = row + current_dir[0]
-                            next_col = col + current_dir[1]
-                            if next_row < 0 or next_col < 0 or next_row >= len(grid) or next_col >= len(grid[0]):
-                                continue
-                            if grid[next_row][next_col] == "1":
-                                queue.append([next_row, next_col])
-                                grid[next_row][next_col] = "0"
-        return islands       
+                    ans += 1
+                    queue = [[i, j]]
+                    while len(queue):
+                        r, c = queue.pop(0)
+                        for d in directions:
+                            nextRow = r + d[0]
+                            nextCol = c + d[1]
+                            if nextRow >= 0 and nextCol >= 0 and nextRow < rows and nextCol < cols and grid[nextRow][nextCol] == "1":
+                                grid[nextRow][nextCol] = "0"
+                                queue.append([nextRow, nextCol])
+        return ans
+                
